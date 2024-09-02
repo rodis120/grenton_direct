e20d6bd7fe772367d0385b17d3bf4b5edc8fbc43
Rogal <progalin55@gmail.com>
2024-09-02 21:44:02 +0000

    Update `light.py`

--- custom_components/grenton_direct/light.py
+++ custom_components/grenton_direct/light.py
@@ -25,35 +25,37 @@ PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend(
     {vol.Required(CONF_OBJ_ID): cv.string, vol.Required(CONF_NAME): cv.string}
 )
 
+RELAY_STATE_INDEX = 0
+
 
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
+    discovery_info: DiscoveryInfoType | None = None,  # noqa: ARG001
 ) -> None:
     """Perform the setup for Light devices."""
-    name = config[CONF_NAME]
-    object_id = config[CONF_OBJ_ID]
     grenton_api = hass.data[DOMAIN][GRENTON_API]
-    add_entities([GrentonLight(grenton_api, object_id, name)], update_before_add=True)
+    add_entities([GrentonLight(grenton_api, config)], update_before_add=True)
 
 
 class GrentonLight(LightEntity):
     """Representation of a GrentonLoght."""
 
-    def __init__(self, grenton_api: CluClient, object_id: str, name: str) -> None:
+    def __init__(self, grenton_api: CluClient, config: ConfigType) -> None:
         """Init GrentonLight."""
         super().__init__()
         self._api = grenton_api
-        self._object_id = object_id
+        self._object_id = config[CONF_OBJ_ID]
 
-        self._attr_name = name
-        self._attr_unique_id = DOMAIN + "." + object_id
+        self._attr_name = config[CONF_NAME]
+        self._attr_unique_id = DOMAIN + "." + self._object_id
         self._attr_color_mode = ColorMode.ONOFF
         self._attr_supported_color_modes = {ColorMode.ONOFF}
 
-        self._api.register_value_change_handler(object_id, 0, self._update_handler)
+        self._api.register_value_change_handler(
+            self._object_id, RELAY_STATE_INDEX, self._update_handler
+        )
 
     def _update_handler(self, ctx: UpdateContext) -> None:
         self._attr_is_on = ctx.value
@@ -64,9 +66,9 @@ class GrentonLight(LightEntity):
     @override
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn on light."""
-        await self._api.set_value_async(self._object_id, 0, 1)
+        await self._api.set_value_async(self._object_id, RELAY_STATE_INDEX, 1)
 
     @override
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Tunr off light."""
-        await self._api.set_value_async(self._object_id, 0, 0)
+        await self._api.set_value_async(self._object_id, RELAY_STATE_INDEX, 0)
