7ff533a2317340672dc0b6b56235145e3ebb6ca6
Rogal <progalin55@gmail.com>
2024-12-03 23:15:00 +0000

    Fix RGBW module WIP

--- custom_components/grenton_direct/light.py
+++ custom_components/grenton_direct/light.py
@@ -2,12 +2,16 @@
 
 from __future__ import annotations
 
-import asyncio
 from typing import TYPE_CHECKING, Any, override
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from homeassistant.components.light import ATTR_RGBW_COLOR, ColorMode, LightEntity
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ATTR_RGBW_COLOR,
+    ColorMode,
+    LightEntity,
+)
 from homeassistant.const import CONF_NAME
 
 from .const import (
@@ -31,6 +35,9 @@ COLOR_RED_INDEX = 3
 COLOR_GREEN_INDEX = 4
 COLOR_BLUE_INDEX = 5
 COLOR_WHITE_INDEX = 15
+HEX_COLOR_INDEX = 6
+SET_WHITE_INDEX = 12
+BRIGHTNESS_INDEX = 0
 
 
 async def async_setup_platform(
@@ -98,48 +105,66 @@ class GrentonRGBW(LightEntity):
         self._attr_name = config[CONF_NAME]
         self._attr_unique_id = DOMAIN + "." + self._object_id
         self._attr_color_mode = ColorMode.RGBW
-        self._attr_supported_color_modes = {ColorMode.RGB, ColorMode.RGBW}
+        self._attr_supported_color_modes = {
+            ColorMode.RGBW,
+            ColorMode.BRIGHTNESS,
+            ColorMode.ONOFF,
+        }
+
+        self._attr_rgbw_color = (0, 0, 0, 0)
 
         self._api.register_value_change_handler(
-            self._object_id, COLOR_RED_INDEX, self._update_handler
-        )
-        self._api.register_value_change_handler(
-            self._object_id, COLOR_GREEN_INDEX, self._update_handler
-        )
-        self._api.register_value_change_handler(
-            self._object_id, COLOR_BLUE_INDEX, self._update_handler
+            self._object_id, HEX_COLOR_INDEX, self._update_handler
         )
         self._api.register_value_change_handler(
             self._object_id, COLOR_WHITE_INDEX, self._update_handler
+        )
+        self._api.register_value_change_handler(
+            self._object_id, BRIGHTNESS_INDEX, self._update_handler
         )
 
     def _update_handler(self, ctx: UpdateContext) -> None:
         r, g, b, w = self._attr_rgbw_color or (0, 0, 0, 0)
 
-        if ctx.index == COLOR_RED_INDEX:
-            self._attr_rgbw_color = (ctx.value, g, b, w)
-        elif ctx.index == COLOR_GREEN_INDEX:
-            self._attr_rgbw_color = (r, ctx.value, b, w)
-        elif ctx.index == COLOR_BLUE_INDEX:
-            self._attr_rgbw_color = (r, g, ctx.value, w)
+        if ctx.index == HEX_COLOR_INDEX:
+            hex_color = ctx.value
+            r = int(hex_color[1:3], base=16)
+            g = int(hex_color[3:5], base=16)
+            b = int(hex_color[5:7], base=16)
+
+            self._attr_rgbw_color = (r, g, b, w)
         elif ctx.index == COLOR_WHITE_INDEX:
             self._attr_rgbw_color = (r, g, b, ctx.value)
+        elif ctx.index == BRIGHTNESS_INDEX:
+            self._attr_brightness = ctx.value
+            self._attr_is_on = ctx.value != 0
+        else:
+            return
+
+        self.schedule_update_ha_state()
 
     @override
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Set RGBW color."""
-        await self._async_set_color(kwargs[ATTR_RGBW_COLOR])
+        color = kwargs.get(ATTR_RGBW_COLOR)
+        brightness = kwargs.get(ATTR_BRIGHTNESS, 0) / 255
+
+        if color:
+            hex_color = "#{:02x}{:02x}{:02x}".format(*color[:3])
+            await self._api.execute_method_async(
+                self._object_id, HEX_COLOR_INDEX, hex_color
+            )
+            await self._api.execute_method_async(
+                self._object_id, SET_WHITE_INDEX, color[3]
+            )
+        elif brightness:
+            await self._api.execute_method_async(
+                self._object_id, BRIGHTNESS_INDEX, brightness
+            )
+        else:
+            await self._api.execute_method_async(self._object_id, BRIGHTNESS_INDEX, 1)
 
     @override
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn off light."""
-        await self._async_set_color((0, 0, 0, 0))
-
-    async def _async_set_color(self, rgbw_color: tuple[int, int, int, int]) -> None:
-        r, g, b, w = rgbw_color
-        await asyncio.gather(
-            self._api.set_value_async(self._object_id, COLOR_RED_INDEX, r),
-            self._api.set_value_async(self._object_id, COLOR_GREEN_INDEX, g),
-            self._api.set_value_async(self._object_id, COLOR_BLUE_INDEX, b),
-            self._api.set_value_async(self._object_id, COLOR_WHITE_INDEX, w),
-        )
+        await self._api.execute_method_async(self._object_id, BRIGHTNESS_INDEX, 0)
